03d51ab459ccc80c56b7b137dfcd71abe035cffc
Aleem Anwer <techteam.it07@gmail.com>
2025-12-28 00:44:43 +0530

    admin routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,8 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
-import { functions,inngest } from "./config/inngest.js";
-
+import { functions, inngest } from "./config/inngest.js";
+import adminRoutes from "./routes/admin.route.js";
 const app = express();
 const __dirname = path.resolve();
 app.use(clerkMiddleware());  //auth object under the request
@@ -16,7 +16,8 @@ app.get("/api/health", (req, res) => {
 app.use(express.json());
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
-app.use("/api/inngest", serve({ client: inngest, functions }));
+  app.use("/api/inngest", serve({ client: inngest, functions }));
+  app.use("/api/admin",adminRoutes);
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
